287b810b92ccaad59375b0838aaaf9ff47a19feb
Alex Price <hi@alexpriceonline.com>
2020-07-03 09:51:36 +0100

    Improved the current state conditional

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,12 @@ async function bootstrap() {
   app.useStaticAssets(join(__dirname, '..', 'public'));
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
   app.setViewEngine('hbs');
+
+  // Register the Handlebars partials and helpers
   hbs.registerPartials(join(__dirname, '..', 'views', 'partials'));
+  hbs.registerHelper('if_eq', (a, b, opts) =>
+    a === b ? opts.fn(this) : opts.inverse(this),
+  );
 
   await app.listen(4001);
 }
